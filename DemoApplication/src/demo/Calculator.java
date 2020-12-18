f7cd0c761e1925273a023fb69fb166111a80be73
Kaviya72 <76148837+Kaviya72@users.noreply.github.com>
2020-12-18 16:46:57 +0530

    product method added

--- DemoApplication/src/demo/Calculator.java
+++ DemoApplication/src/demo/Calculator.java
@@ -3,6 +3,10 @@ package demo;
 public class Calculator {
 	public void add(int num1,int num2) {
 		System.out.println("summation: " + num1+num2);
+	}
+	public void prod(int num1,int num2) {
+		System.out.println("product: " +( num1*num2));
 
 	}
+
 }
