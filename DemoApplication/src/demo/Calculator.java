e050b422fe0b811081246e59db78f7430c34009a
Kaviya72 <76148837+Kaviya72@users.noreply.github.com>
2020-12-18 16:32:50 +0530

    modified add in master

--- DemoApplication/src/demo/Calculator.java
+++ DemoApplication/src/demo/Calculator.java
@@ -2,10 +2,6 @@ package demo;
 
 public class Calculator {
 	public void add(int num1,int num2) {
-		System.out.println("sum: " + (num1+num2));
-	}
-	
-	public void product(int num1,int num2) {
-		System.out.println("Product: " + (num1*num2));
+		("sum: " + (num1+num2));
 	}
 }
